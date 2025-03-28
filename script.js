9e8289416d6c92a9d6c9a5068b22e5824097ebc2
petronela <delyoara@yahoo.fr>
2025-03-28 13:32:00 +0100

    "Récupérer les coordonnées de la ville"

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 // A toi de jouer pour cette partie :-) Happy coding !
+
+const city = document.getElementById('city-search');
+const button = document.querySelector('button');
+const coordGPS = document.querySelector('#gps')
+const cityName = document.querySelector('#city')
+
+async function researchCity (cityValue) {
+    
+    const response = await fetch (`https://nominatim.openstreetmap.org/search?q=${cityValue}&format=json&addressdetails=1&limit=1`)
+    const data = await response.json();
+    return data; 
+    console.log(data);
+}
+
+button.addEventListener('click', async() => {
+    button.setAttribute("id", "searchButton");
+    const cityValue = cityInput.value;
+    console.log("Ville : ", cityValue); 
+
+    const data = await researchCity(cityValue);
+    if (data.length > 0) {
+        const lat = data[0].lat;
+        const lon = data[0].lon;
+        coordGPS.innerHTML = `Coordonées : ${lat}, ${lon}`
+        console.log(`Latitude: ${lat}, Longitude: ${lon}`);
+        cityName.innerHTML = cityValue
+
+    } 
+})
+
+
